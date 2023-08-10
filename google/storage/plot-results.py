c0362f9affbf152e157b6d9678d9aac8e11cb6e7
vsoch <vsoch@users.noreply.github.com>
2023-08-09 18:09:05 -0600

    run black
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- google/storage/plot-results.py
+++ google/storage/plot-results.py
@@ -16,8 +16,8 @@ data_dir = os.path.join(here, "data")
 
 plt.style.use("bmh")
 
-def main():
 
+def main():
     results = {}
 
     # First read in the raw data
@@ -30,38 +30,41 @@ def main():
         results[filename] = res
 
     # Get fields from first file (should all be the same)
-    job = list(results.values())[0]['data'][0]['jobs'][0]
-    sections = {"read": job['read'], "write": job['write'], 'trim': job['trim']}
+    job = list(results.values())[0]["data"][0]["jobs"][0]
+    sections = {"read": job["read"], "write": job["write"], "trim": job["trim"]}
     columns = []
     for name, section in sections.items():
         for field, value in section.items():
             if isinstance(value, (float, int)):
-                columns.append(f"{name}_{field}")        
+                columns.append(f"{name}_{field}")
 
     # Create a data frame
-    df = pandas.DataFrame(columns=['uid', 'storage'] + columns)
+    df = pandas.DataFrame(columns=["uid", "storage"] + columns)
 
     # Get column names for some random metrics that maybe are useful?
     idx = 0
     for filename, res in results.items():
-        storage, ext = filename.rsplit('-', 1)
-        uid = int(ext.split('.')[0])
-        job = res['data'][0]['jobs'][0]
-        sections = {"read": job['read'], "write": job['write'], 'trim': job['trim']}
+        storage, ext = filename.rsplit("-", 1)
+        uid = int(ext.split(".")[0])
+        job = res["data"][0]["jobs"][0]
+        sections = {"read": job["read"], "write": job["write"], "trim": job["trim"]}
         row = {}
         # This assumes in the same order
         for name, section in sections.items():
             for field, value in section.items():
                 if isinstance(value, (float, int)):
                     row[f"{name}_{field}"] = value
-        df.loc[idx, ["uid", "storage"] + list(row.keys())] = [uid, storage] + list(row.values())            
-        idx +=1        
+        df.loc[idx, ["uid", "storage"] + list(row.keys())] = [uid, storage] + list(
+            row.values()
+        )
+        idx += 1
 
     # Save data
     df.to_csv(os.path.join(data_dir, "data.csv"))
 
     # And plot!
     plot_results(df)
+
 
 def plot_results(df):
     """
@@ -71,10 +74,10 @@ def plot_results(df):
     img = os.path.join(here, "img")
     if not os.path.exists(img):
         os.makedirs(img)
-   
+
     # Assemble a data frame with all the result types
     for column in df.columns:
-        if column in ['storage', 'uid']:
+        if column in ["storage", "uid"]:
             continue
         if df[column].sum() == 0:
             print(f"Metric {column} is all zeros, skipping.")
@@ -82,8 +85,8 @@ def plot_results(df):
 
         # Save to data file
         title = f"Result for {column}"
-        slug = column.replace(' ', '-')
-        
+        slug = column.replace(" ", "-")
+
         # for sty in plt.style.available:
         ax = sns.boxplot(data=df, x="storage", y=column, hue="storage")
         plt.title(title)
@@ -98,9 +101,7 @@ def plot_results(df):
         # And distribution
         sns.histplot(df, x=column, hue="storage")
         plt.title(f"Histogram for result {column}")
-        plt.savefig(
-            os.path.join(img, f"{slug}-hist.png"), dpi=300, bbox_inches="tight"
-        )
+        plt.savefig(os.path.join(img, f"{slug}-hist.png"), dpi=300, bbox_inches="tight")
         plt.clf()
         plt.close()
 
