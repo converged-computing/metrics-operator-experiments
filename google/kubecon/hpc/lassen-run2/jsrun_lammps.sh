b1a62c0280e46e76377411c9dbb20fe785547af9
vsoch <vsoch@users.noreply.github.com>
2023-10-03 14:58:04 -0600

    update submit script
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- google/kubecon/hpc/lassen-run2/jsrun_lammps.sh
+++ google/kubecon/hpc/lassen-run2/jsrun_lammps.sh
@@ -13,12 +13,20 @@ date; hostname
 echo -n 'JobID is '; echo $LSB_JOBID
 echo "Hosts: $LSB_HOSTS"
 export PATH=$PATH:/g/g0/sochat1/kubecon-2023/lammps/bin/bin
-cd /g/g0/sochat1/kubecon-2023/lammpps/examples/reaxff/HNS/
+cd /g/g0/sochat1/kubecon-2023/lammps/examples/reaxff/HNS/
 mkdir -p /p/gpfs1/sochat1/kubecon/
 
 # What I tried on an allocation of 2 nodes
 # jsrun lmp -v x 2 -v y 2 -v z 2 -in in.reaxc.hns -nocite
 lmp=/g/g0/sochat1/kubecon-2023/lammps/bin/bin/lmp
+problem="-v x 64 -v y 16 -v z 16 -in in.reaxc.hns -nocite"
+
+# Command from milroy
+# jsrun -p 40 --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu /g/g0/sochat1/kubecon-2023/lammps/bin/bin/lmp -v x 2 -v y 2 -v z 2 -in in.reaxc.hns -nocite
+# time -p jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem} >> ${outfile} &
+
+# To submit
+# bsub < jsrun_lammps.sh
 
 # TODO I didn't change jsrun arguments because I don't have a clue
 # We need to make sure we only use this number of ranks
@@ -31,15 +39,14 @@ do
     do
         echo "==============" >> $outfile
         echo "Start run ${i} of 20" >> $outfile
-        echo -e "\ntime jsrun -n $ranks -a 1 -c 1 -r 1 -l cpu-cpu ${lmp}" >> ${output}
-        { time -p jsrun -n $ranks -a 1 -c 1 -r 1 -l cpu-cpu ${lmp}; } >> ${output} 2>&1
-        echo -e "\ntime jsrun -n 2 -a 1 -c 1 -r 1 -l cpu-cpu ${lmp}" >> ${output}
-        { time -p jsrun -n 2 -a 1 -c 1 -r 1 -l cpu-cpu ${lmp} ; } >> ${output} 2>&1
-        echo -e "\ntime jsrun -n $ranks -a 1 -c 1 -r 1 -l cpu-cpu ${lmp}" >> $output
-        { time -p jsrun -n $ranks -a 1 -c 1 -r 1 -l cpu-cpu ${lmp} ; } >> $output 2>&1
-        echo -e "\ntime jsrun -n $ranks -a 1 -c 1 -r 1 -l cpu-cpu ${lmp}" >> $output
-        { time -p jsrun -n $ranks -a 1 -c 1 -r 1 -l cpu-cpu ${lmp} ; } >> $output 2>&1
-        echo -e "End run ${i} of 20\n" >> $output
+        echo -e "\ntime jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem}" >> ${outfile}
+        time -p jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem} >> ${outfile} &
+        echo -e "\ntime jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem} " >> ${outfile}
+        time -p jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem} >> ${outfile} &
+        echo -e "\ntime jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem}" >> ${outfile}
+        time -p jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem} >> ${outfile} &
+        echo -e "\ntime jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem}" >> ${outfile}
+        time -p jsrun -p $ranks --rs_per_socket=1 -c 20 -r 2 -l cpu-cpu ${lmp} ${problem} >> ${outfile}
+        echo -e "End run ${i} of 20\n" >> ${outfile}
     done
 done
-
